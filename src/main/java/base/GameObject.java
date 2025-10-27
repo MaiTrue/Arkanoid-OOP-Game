4e2d6c522e6954f107a550013a4921cf9679c2a7
quockhanh30 <ncuockhanh@gmail.com>
2025-10-27 15:27:45 +0700

    Cập nhật Renderer và GameObject

--- src/main/java/base/GameObject.java
+++ src/main/java/base/GameObject.java
@@ -7,8 +7,10 @@ import javafx.scene.canvas.GraphicsContext;
  * chứa vị trí và kích thước.
  */
 public abstract class GameObject {
-    protected double x, y;
-    protected double width, height;
+    protected double x;
+    protected double y;
+    protected double width;
+    protected double height;
 
     public GameObject(double x, double y, double width, double height) {
         this.x = x; this.y = y; this.width = width; this.height = height;
---
+++ src/main/java/core/Renderer.java
@@ -0,0 +1,17 @@
+package core;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Renderer {
+    private GraphicsContext gc;
+
+    public Renderer(GraphicsContext gc) {
+        this.gc = gc;
+    }
+
+    public void clear() {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 1000, 770);
+    }
+}
